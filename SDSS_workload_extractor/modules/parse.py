ddce4ea882ea52554f4ceecc5e8524aa4390e8bd
Alberto <aabello@essi.upc.edu>
2025-08-10 22:23:45 +0200

    More keywords added to the list

--- SDSS_workload_extractor/modules/parse.py
+++ SDSS_workload_extractor/modules/parse.py
@@ -159,9 +159,9 @@ def extract_query_info(real_query):
                     comparisons.append({"attribute": attribute, "operator": operator})
                 elif current.ttype is Keyword:
                     # TODO: Consider other more complex comparisons not properly treated by sqlparse library
-                    #       By now, they are simply ignored and conjuntive query assumed
-                    if current.value.upper() not in ("WHERE", "AND", "NOT", "IN", "OR", "IS", "NULL", "NOT NULL", "GO", "ABS"):
-                        print(f"New unexpected keyword {current.value} detected in {sql_query}")
+                    #       By now, these keywords are simply ignored (not used as attribute or table names) and conjuntive query assumed
+                    if current.value.upper() not in ("WHERE", "AND", "NOT", "IN", "OR", "IS", "NULL", "NOT NULL", "GO", "ABS", "KEY", "INCLUDE"):
+                        print(f"New unexpected keyword '{current.value}' detected in {sql_query}")
                         return None, None
         elif token.is_group:
             has_nested_queries = True
