e5b4f00d870946cdc32e9a77a0d938a03e749c28
Alberto <aabello@essi.upc.edu>
2025-09-08 19:16:25 +0200

    Added the processing of lists to the bat file

--- EDBT_running_example.bat
+++ EDBT_running_example.bat
@@ -36,10 +36,18 @@ python queryExecutor.py --hide_warnings --hide_progress --paradigm NF2_JSON --db
 
 echo ========================================================================== Optimized version based on the workload of May 2025 in NF2 with an intermediate table for the association
 echo -------------------------------------------------------------------------- Database creation
-python catalogAction.py --hide_warnings --hide_progress --dbconf_file db_conf_unibo.txt --dbschema dorm_edbt_NF2_opt202505_withTable --supersede --create design --paradigm NF2_JSON --dsg_fmt XML --dsg_spec 1NF/SDSS_simple_opt202505_withTable  --src_sch dorm_edbt_baseline --src_kind 1NF
+python catalogAction.py --hide_warnings --hide_progress --dbconf_file db_conf_unibo.txt --dbschema dorm_edbt_opt202505_NF2_withTable --supersede --create design --paradigm NF2_JSON --dsg_fmt XML --dsg_spec 1NF/SDSS_simple_opt202505_withTable --src_sch dorm_edbt_baseline --src_kind 1NF
 echo .......................................................................... Get query performance for workload of May 2025
 python queryExecutor.py --hide_warnings --hide_progress --paradigm NF2_JSON --dbconf_file db_conf_unibo.txt --dbschema dorm_edbt_opt202505_NF2_withTable --save_cost --cost_file EDBT_running_example_costs/opt202505_NF2_withTable_202505 --query_file files/queries/SDSS_2505
 echo .......................................................................... Get query performance for workload of June 2025
 python queryExecutor.py --hide_warnings --hide_progress --paradigm NF2_JSON --dbconf_file db_conf_unibo.txt --dbschema dorm_edbt_opt202505_NF2_withTable --save_cost --cost_file EDBT_running_example_costs/opt202505_NF2_withTable_202506 --query_file files/queries/SDSS_2506
+
+echo ========================================================================== Optimized version based on the workload of May 2025 in NF2 with a list of FKs for the association
+echo -------------------------------------------------------------------------- Database creation
+python catalogAction.py --hide_warnings --hide_progress --dbconf_file db_conf_unibo.txt --dbschema dorm_edbt_opt202505_NF2_withList --supersede --create design --paradigm NF2_JSON --dsg_fmt XML --dsg_spec NF2/SDSS_simple_opt202505_withList --src_sch dorm_edbt_baseline --src_kind 1NF
+echo .......................................................................... Get query performance for workload of May 2025
+python queryExecutor.py --hide_warnings --hide_progress --paradigm NF2_JSON --dbconf_file db_conf_unibo.txt --dbschema dorm_edbt_opt202505_NF2_withList --save_cost --cost_file EDBT_running_example_costs/opt202505_NF2_withList_202505 --query_file files/queries/SDSS_2505
+echo .......................................................................... Get query performance for workload of June 2025
+python queryExecutor.py --hide_warnings --hide_progress --paradigm NF2_JSON --dbconf_file db_conf_unibo.txt --dbschema dorm_edbt_opt202505_NF2_withList --save_cost --cost_file EDBT_running_example_costs/opt202505_NF2_withList_202506 --query_file files/queries/SDSS_2506
 
 echo Ending time: %TIME%
