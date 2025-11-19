a127434b92c9d6fcdbb44107946277997964d9a4
Alberto <aabello@essi.upc.edu>
2025-11-19 09:31:48 +0100

    Query cost calculation added to test_all_NF2.bat

--- test_all_NF2.bat
+++ test_all_NF2.bat
@@ -3,13 +3,13 @@ echo ========================================================================== 
 echo -------------------------------------------------------------------------- Table creation
 python catalogAction.py --hide_progress --dbconf_file db_conf.txt --check --supersede --create design --paradigm NF2_JSON --dsg_fmt XML --dsg_spec 1NF/artist-record-track
 echo .......................................................................... Query execution
-python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --paradigm NF2_JSON --query_file files/queries/artist-record-track.json
+python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --save_cost --paradigm NF2_JSON --query_file files/queries/artist-record-track.json
 
 echo ========================================================================== 1NF/artist-record-track_test1
 echo -------------------------------------------------------------------------- Table creation
 python catalogAction.py --hide_progress --dbconf_file db_conf.txt --check --supersede --create design --paradigm NF2_JSON --dsg_fmt XML --dsg_spec 1NF/artist-record-track_test1
 echo .......................................................................... Query execution
-python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --paradigm NF2_JSON --query_file files/queries/artist-record-track.json
+python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --save_cost --paradigm NF2_JSON --query_file files/queries/artist-record-track.json
 
 echo ========================================================================== Creation of source schema with design 1NF/book-authors_test2
 echo -------------------------------------------------------------------------- Table creation at source schema
@@ -21,69 +21,69 @@ echo ========================================================================== 
 echo -------------------------------------------------------------------------- Table creation
 python catalogAction.py --hide_progress --dbconf_file db_conf.txt --check --supersede --create design --paradigm NF2_JSON --dsg_fmt XML --dsg_spec 1NF/book-authors --src_sch source --src_kind NF2_JSON
 echo .......................................................................... Query execution
-python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --paradigm NF2_JSON --query_file files/queries/book-authors.json
+python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --save_cost --paradigm NF2_JSON --query_file files/queries/book-authors.json
 
 echo ========================================================================== 1NF/book-authors_test1
 echo -------------------------------------------------------------------------- Table creation
 python catalogAction.py --hide_progress --dbconf_file db_conf.txt --check --supersede --create design --paradigm NF2_JSON --dsg_fmt XML --dsg_spec 1NF/book-authors_test1 --src_sch source --src_kind NF2_JSON
 echo .......................................................................... Query execution
-python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --paradigm NF2_JSON --query_file files/queries/book-authors.json
+python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --save_cost --paradigm NF2_JSON --query_file files/queries/book-authors.json
 
 echo ========================================================================== 1NF/book-authors_test2
 echo -------------------------------------------------------------------------- Table creation
 python catalogAction.py --hide_progress --dbconf_file db_conf.txt --check --supersede --create design --paradigm NF2_JSON --dsg_fmt XML --dsg_spec 1NF/book-authors_test2 --src_sch source --src_kind NF2_JSON
 echo .......................................................................... Query execution
-python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --paradigm NF2_JSON --query_file files/queries/book-authors.json
+python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --save_cost --paradigm NF2_JSON --query_file files/queries/book-authors.json
 
 echo ========================================================================== 1NF/book-authors_test3
 echo -------------------------------------------------------------------------- Table creation
 python catalogAction.py --hide_progress --dbconf_file db_conf.txt --check --supersede --create design --paradigm NF2_JSON --dsg_fmt XML --dsg_spec 1NF/book-authors_test3 --src_sch source --src_kind NF2_JSON
 echo .......................................................................... Query execution
-python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --paradigm NF2_JSON --query_file files/queries/book-authors.json
+python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --save_cost --paradigm NF2_JSON --query_file files/queries/book-authors.json
 
 echo ========================================================================== 1NF/book-authors_test4
 echo -------------------------------------------------------------------------- Table creation
 python catalogAction.py --hide_progress --dbconf_file db_conf.txt --check --supersede --create design --paradigm NF2_JSON --dsg_fmt XML --dsg_spec 1NF/book-authors_test4 --src_sch source --src_kind NF2_JSON
 echo .......................................................................... Query execution
-python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --paradigm NF2_JSON --query_file files/queries/book-authors.json
+python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --save_cost --paradigm NF2_JSON --query_file files/queries/book-authors.json
 
 echo ========================================================================== NF2/book-authors_test1
 echo -------------------------------------------------------------------------- Table creation
 python catalogAction.py --hide_progress --dbconf_file db_conf.txt --check --supersede --create design --paradigm NF2_JSON --dsg_fmt XML --dsg_spec NF2/book-authors_test1 --src_sch source --src_kind NF2_JSON
 echo .......................................................................... Query execution
-python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --paradigm NF2_JSON --query_file files/queries/book-authors.json
+python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --save_cost --paradigm NF2_JSON --query_file files/queries/book-authors.json
 
 echo ========================================================================== NF2/book-authors_test2
 echo -------------------------------------------------------------------------- Table creation
 python catalogAction.py --hide_progress --dbconf_file db_conf.txt --check --supersede --create design --paradigm NF2_JSON --dsg_fmt XML --dsg_spec NF2/book-authors_test2 --src_sch source --src_kind NF2_JSON
 echo .......................................................................... Query execution
-python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --paradigm NF2_JSON --query_file files/queries/book-authors.json
+python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --save_cost --paradigm NF2_JSON --query_file files/queries/book-authors.json
 
 echo ========================================================================== NF2/book-authors_test3
 echo -------------------------------------------------------------------------- Table creation
 python catalogAction.py --hide_progress --dbconf_file db_conf.txt --check --supersede --create design --paradigm NF2_JSON --dsg_fmt XML --dsg_spec NF2/book-authors_test3 --src_sch source --src_kind NF2_JSON
 echo .......................................................................... Query execution
-python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --paradigm NF2_JSON --query_file files/queries/book-authors.json
+python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --save_cost --paradigm NF2_JSON --query_file files/queries/book-authors.json
 
 echo ========================================================================== 1NF/students-workers_AllInSuperclassTable
 python catalogAction.py --hide_progress --dbconf_file db_conf.txt --check --supersede --create design --paradigm NF2_JSON --dsg_fmt XML --dsg_spec 1NF/students-workers_AllInSuperclassTable
 echo .......................................................................... Query execution
-python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --paradigm NF2_JSON --query_file files/queries/students-workers.json
+python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --save_cost --paradigm NF2_JSON --query_file files/queries/students-workers.json
 
 echo ========================================================================== 1NF/students-workers_OneClassOneTable
 python catalogAction.py --hide_progress --dbconf_file db_conf.txt --check --supersede --create design --paradigm NF2_JSON --dsg_fmt XML --dsg_spec 1NF/students-workers_OneClassOneTable
 echo .......................................................................... Query execution
-python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --paradigm NF2_JSON --query_file files/queries/students-workers.json
+python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --save_cost --paradigm NF2_JSON --query_file files/queries/students-workers.json
 
 echo ========================================================================== 1NF/students-workers_OneTablePerSubclass
 python catalogAction.py --hide_progress --dbconf_file db_conf.txt --check --supersede --create design --paradigm NF2_JSON --dsg_fmt XML --dsg_spec 1NF/students-workers_OneTablePerSubclass
 echo .......................................................................... Query execution
-python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --paradigm NF2_JSON --query_file files/queries/students-workers.json
+python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --save_cost --paradigm NF2_JSON --query_file files/queries/students-workers.json
 
 echo ========================================================================== 1NF/students-workers_OneTableForTheIntersection
 python catalogAction.py --hide_progress --dbconf_file db_conf.txt --check --supersede --create design --paradigm NF2_JSON --dsg_fmt XML --dsg_spec 1NF/students-workers_OneTableForTheIntersection
 echo .......................................................................... Query execution
-python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --paradigm NF2_JSON --query_file files/queries/students-workers.json
+python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --save_cost --paradigm NF2_JSON --query_file files/queries/students-workers.json
 
 echo ========================================================================== Creation of source schema with design 1NF/book-authors-topic
 echo -------------------------------------------------------------------------- Table creation at source2 schema
@@ -95,30 +95,30 @@ echo ========================================================================== 
 echo -------------------------------------------------------------------------- Table creation
 python catalogAction.py --hide_progress --dbconf_file db_conf.txt --check --supersede --create design --paradigm NF2_JSON --dsg_fmt XML --dsg_spec 1NF/book-authors-topic --src_sch source2 --src_kind 1NF
 echo .......................................................................... Query execution
-python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --paradigm NF2_JSON --query_file files/queries/book-authors-topic.json
+python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --save_cost --paradigm NF2_JSON --query_file files/queries/book-authors-topic.json
 
 echo ========================================================================== 1NF/book-authors-topic_test1
 echo -------------------------------------------------------------------------- Table creation
 python catalogAction.py --hide_progress --dbconf_file db_conf.txt --check --supersede --create design --paradigm NF2_JSON --dsg_fmt XML --dsg_spec 1NF/book-authors-topic_test1 --src_sch source2 --src_kind 1NF
 echo .......................................................................... Query execution
-python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --paradigm NF2_JSON --query_file files/queries/book-authors-topic.json
+python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --save_cost --paradigm NF2_JSON --query_file files/queries/book-authors-topic.json
 
 echo ========================================================================== 1NF/book-authors-topic_partitioned
 python catalogAction.py --hide_progress --dbconf_file db_conf.txt --check --supersede --create design --paradigm NF2_JSON --dsg_fmt XML --dsg_spec 1NF/book-authors-topic_partitioned --src_sch source2 --src_kind 1NF
 echo .......................................................................... Query execution
-python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --paradigm NF2_JSON --query_file files/queries/book-authors-topic.json
+python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --save_cost --paradigm NF2_JSON --query_file files/queries/book-authors-topic.json
 
 echo ========================================================================== NF2/book-authors-topic_test1
 python catalogAction.py --hide_progress --dbconf_file db_conf.txt --check --supersede --create design --paradigm NF2_JSON --dsg_fmt XML --dsg_spec NF2/book-authors-topic_test1 --src_sch source2 --src_kind 1NF
 echo .......................................................................... Query execution
-python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --paradigm NF2_JSON --query_file files/queries/book-authors-topic.json
+python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --save_cost --paradigm NF2_JSON --query_file files/queries/book-authors-topic.json
 
 echo ========================================================================== NF2/book-authors-topic_test2
 python catalogAction.py --hide_progress --dbconf_file db_conf.txt --check --supersede --create design --paradigm NF2_JSON --dsg_fmt XML --dsg_spec NF2/book-authors-topic_test2 --src_sch source2 --src_kind 1NF
 echo .......................................................................... Query execution
-python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --paradigm NF2_JSON --query_file files/queries/book-authors-topic.json
+python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --save_cost --paradigm NF2_JSON --query_file files/queries/book-authors-topic.json
 
 echo ========================================================================== NF2/book-authors-topic_test3
 python catalogAction.py --hide_progress --dbconf_file db_conf.txt --check --supersede --create design --paradigm NF2_JSON --dsg_fmt XML --dsg_spec NF2/book-authors-topic_test3 --src_sch source2 --src_kind 1NF
 echo .......................................................................... Query execution
-python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --paradigm NF2_JSON --query_file files/queries/book-authors-topic.json
+python queryExecutor.py --hide_progress --dbconf_file db_conf.txt --save_cost --paradigm NF2_JSON --query_file files/queries/book-authors-topic.json
